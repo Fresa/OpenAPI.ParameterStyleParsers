f9d7c1585bb2198b3213aee37791dcff0da58722
Fredrik Arvidsson <fresa@fresa.se>
2025-12-13 12:11:29 +0100

    fix(openapi 2.0): catch parsing problems when parsing primitives

--- src/OpenAPI.ParameterStyleParsers/OpenApi20/ParameterParsers/Primitive/PrimitiveJsonConverter.cs
+++ src/OpenAPI.ParameterStyleParsers/OpenApi20/ParameterParsers/Primitive/PrimitiveJsonConverter.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 
 namespace OpenAPI.ParameterStyleParsers.OpenApi20.ParameterParsers.Primitive;
@@ -11,6 +12,21 @@ internal static class PrimitiveJsonConverter
         out JsonNode? instance,
         [NotNullWhen(false)] out string? error)
     {
+        string GetWrongTypeError() => $"Value {value} is not a {jsonType}";
+        bool TryParse([NotNullWhen(true)] out JsonNode? jsonNode)
+        {
+            try
+            {
+                jsonNode = JsonNode.Parse(value);
+                return jsonNode != null;
+            }
+            catch (JsonException)
+            {
+                jsonNode = null;
+                return false;
+            }
+        }
+        
         switch (jsonType)
         {
             case Parameter.Types.String:
@@ -18,11 +34,42 @@ internal static class PrimitiveJsonConverter
                 error = null;
                 return true;
             case Parameter.Types.Number:
+                if (TryParse(out instance) &&
+                    instance.GetValueKind() is JsonValueKind.Number)
+                {
+                    error = null;
+                    return true;
+                }
+
+                instance = null;
+                error = GetWrongTypeError();
+                return false;
             case Parameter.Types.Boolean:
+                if (TryParse(out instance) &&
+                    instance.GetValueKind() is JsonValueKind.False or JsonValueKind.True)
+                {
+                    error = null;
+                    return true;
+                }
+
+                instance = null;
+                error = GetWrongTypeError();
+                return false;
             case Parameter.Types.Integer:
-                instance = JsonNode.Parse(value);
-                error = null;
-                return true;
+                if (TryParse(out instance) &&
+                    instance.GetValueKind() is JsonValueKind.Number &&
+                    (value == "-0" || 
+                     value.EndsWith(".0") || 
+                     (long.TryParse(value, out var integer) && 
+                      integer is <= 9007199254740992 and > -9007199254740992)))
+                {
+                    error = null;
+                    return true;
+                }
+
+                instance = null;
+                error = GetWrongTypeError();
+                return false;
             default:
                 error = $"Json type {jsonType} is not a primitive type, expected one of {string.Join(", ", Parameter.Types.Primitives)}";
                 instance = null;
---
+++ tests/OpenAPI.ParameterStyleParsers.UnitTests/OpenAPI_20/PrimitiveJsonConverterTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+using OpenAPI.ParameterStyleParsers.OpenApi20.ParameterParsers.Primitive;
+
+namespace OpenAPI.ParameterStyleParsers.UnitTests.OpenAPI_20;
+
+public class PrimitiveJsonConverterTests
+{
+    [Theory]
+    [InlineData("integer", "one")]
+    [InlineData("integer", "false")]
+    [InlineData("integer", "1.5")]
+    [InlineData("integer",  "-9007199254740992")]
+    [InlineData("integer",  "9007199254740993")]
+    [InlineData("number", "one")]
+    [InlineData("number", "false")]
+    [InlineData("boolean", "test")]
+    [InlineData("boolean", "1")]
+    public void InvalidTypes_Converting_ReportError(string type, string value)
+    {
+        PrimitiveJsonConverter.TryConvert(value, type, out var instance, out var error)
+            .Should().BeFalse();
+        instance.Should().BeNull();
+        error.Should().NotBeNullOrEmpty();
+    }
+    
+    [Theory]
+    [InlineData("integer", "1", JsonValueKind.Number)]
+    [InlineData("integer", "1.0", JsonValueKind.Number)]
+    [InlineData("integer", "-0", JsonValueKind.Number)]
+    [InlineData("integer",  "-9007199254740991", JsonValueKind.Number)]
+    [InlineData("integer",  "9007199254740992", JsonValueKind.Number)]
+    [InlineData("number", "1", JsonValueKind.Number)]
+    [InlineData("number", "1.0", JsonValueKind.Number)]
+    [InlineData("number", "-0", JsonValueKind.Number)]
+    [InlineData("number",  "-9007199254740991", JsonValueKind.Number)]
+    [InlineData("number",  "9007199254740992", JsonValueKind.Number)]
+    [InlineData("number",  "1.5", JsonValueKind.Number)]
+    [InlineData("boolean",  "true", JsonValueKind.True)]
+    [InlineData("boolean",  "false", JsonValueKind.False)]
+    [InlineData("string",  "false", JsonValueKind.String)]
+    [InlineData("string",  "foo", JsonValueKind.String)]
+    [InlineData("string",  "1", JsonValueKind.String)]
+    [InlineData("string",  "", JsonValueKind.String)]
+    [InlineData("string",  "1.5", JsonValueKind.String)]
+    public void ValidTypes_Converting_ConvertsSuccessfully(string type, string value, JsonValueKind jsonValueKind)
+    {
+        PrimitiveJsonConverter.TryConvert(value, type, out var instance, out var error)
+            .Should().BeTrue();
+        error.Should().BeNull();
+        instance.Should().NotBeNull();
+        instance.ToJsonString().Trim('"').Should().Be(value);
+        instance.GetValueKind().Should().Be(jsonValueKind);
+    }
+}
