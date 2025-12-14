826534918feea8d643a91771f396eb6adb717b01
Fredrik Arvidsson <fresa@fresa.se>
2025-12-14 11:56:05 +0100

    fix(openapi 3.1): convert any styled value to json no matter the type as the input value might not correspond

--- tests/OpenAPI.ParameterStyleParsers.UnitTests/PrimitiveJsonConverterTests.cs
+++ tests/OpenAPI.ParameterStyleParsers.UnitTests/PrimitiveJsonConverterTests.cs
@@ -6,42 +6,52 @@ namespace OpenAPI.ParameterStyleParsers.UnitTests;
 public class PrimitiveJsonConverterTests
 {
     [Theory]
-    [InlineData(InstanceType.Integer, "one")]
-    [InlineData(InstanceType.Number, "one")]
-    [InlineData(InstanceType.Boolean, "one")]
-    public void InvalidTypes_Converting_ReportError(InstanceType type, string value)
-    {
-        PrimitiveJsonConverter.TryConvert(value, type, out var instance, out var error)
-            .Should().BeFalse();
-        instance.Should().BeNull();
-        error.Should().NotBeNullOrEmpty();
-    }
-    
-    [Theory]
-    [InlineData(InstanceType.Integer, "1")]
-    [InlineData(InstanceType.Integer, "1.0")]
-    [InlineData(InstanceType.Integer, "-0")]
-    [InlineData(InstanceType.Integer,  "-9007199254740991")]
-    [InlineData(InstanceType.Integer,  "9007199254740992")]
-    [InlineData(InstanceType.Number, "1")]
-    [InlineData(InstanceType.Number, "1.0")]
-    [InlineData(InstanceType.Number, "-0")]
-    [InlineData(InstanceType.Number,  "-9007199254740991")]
-    [InlineData(InstanceType.Number,  "9007199254740992")]
-    [InlineData(InstanceType.Number,  "1.5")]
-    [InlineData(InstanceType.Boolean,  "true")]
-    [InlineData(InstanceType.Boolean,  "false")]
-    [InlineData(InstanceType.String,  "false")]
-    [InlineData(InstanceType.String,  "foo")]
-    [InlineData(InstanceType.String,  "1")]
-    [InlineData(InstanceType.String,  "")]
-    [InlineData(InstanceType.String,  "1.5")]
-    public void ValidTypes_Converting_ConvertsSuccessfully(InstanceType type, string value)
+    [InlineData(InstanceType.Integer, "1", "1")]
+    [InlineData(InstanceType.Integer, "1.0", "1.0")]
+    [InlineData(InstanceType.Integer, "-0", "-0")]
+    [InlineData(InstanceType.Integer,  "-9007199254740991", "-9007199254740991")]
+    [InlineData(InstanceType.Integer,  "9007199254740992", "9007199254740992")]
+    // out-of-bound for integer type, still formatted as integer as there is no validation
+    [InlineData(InstanceType.Integer,  "9007199254740993", "9007199254740993")]
+    // convert to string if the value is not an integer, but in fact looks like a string
+    [InlineData(InstanceType.Integer,  "foo", "\"foo\"")]
+    [InlineData(InstanceType.Number, "1", "1")]
+    [InlineData(InstanceType.Number, "1.0", "1.0")]
+    [InlineData(InstanceType.Number, "-0", "-0")]
+    [InlineData(InstanceType.Number,  "-9007199254740991", "-9007199254740991")]
+    [InlineData(InstanceType.Number,  "9007199254740992", "9007199254740992")]
+    [InlineData(InstanceType.Number,  "1.5", "1.5")]
+    // convert to string if the value is not a number, but in fact looks like a string
+    [InlineData(InstanceType.Number,  "foo", "\"foo\"")]
+    [InlineData(InstanceType.Boolean,  "true", "true")]
+    [InlineData(InstanceType.Boolean,  "false", "false")]
+    // convert to string if the value is not a boolean, but in fact looks like a string
+    [InlineData(InstanceType.Boolean,  "foo", "\"foo\"")]
+    [InlineData(InstanceType.String,  "false", "\"false\"")]
+    [InlineData(InstanceType.String,  "foo", "\"foo\"")]
+    [InlineData(InstanceType.String,  "1", "\"1\"")]
+    [InlineData(InstanceType.String,  "", "\"\"")]
+    [InlineData(InstanceType.String,  "1.5", "\"1.5\"")]
+    public void ValidTypes_Converting_ConvertsSuccessfully(InstanceType type, string value, string jsonValue)
     {
         PrimitiveJsonConverter.TryConvert(value, type, out var instance, out var error)
             .Should().BeTrue();
         error.Should().BeNull();
         instance.Should().NotBeNull();
-        instance.ToJsonString().Trim('"').Should().Be(value);
+        instance.ToJsonString().Should().Be(jsonValue);
+    }
+    
+    [Theory]
+    [InlineData(InstanceType.Integer,  null)]
+    [InlineData(InstanceType.Number,  null)]
+    [InlineData(InstanceType.Boolean,  null)]
+    [InlineData(InstanceType.String,  null)]
+    [InlineData(InstanceType.Null,  null)]
+    public void Null_Converting_ConvertsSuccessfully(InstanceType type, string value)
+    {
+        PrimitiveJsonConverter.TryConvert(value, type, out var instance, out var error)
+            .Should().BeTrue();
+        error.Should().BeNull();
+        instance.Should().BeNull();
     }
 }
